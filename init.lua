6958b34b729afbcd83f267979cf8d5bf728039ce
Szymon Haponiuk <szymon.haponiuk@nomagic.ai>
2025-07-11 17:25:07 +0200

    remove hjkl restriction

--- init.lua
+++ init.lua
@@ -186,10 +186,10 @@ vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
 vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
 vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
 vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
-vim.keymap.set('n', 'j', '<cmd>echo "sorry"<CR>')
-vim.keymap.set('n', 'k', '<cmd>echo "sorry"<CR>')
-vim.keymap.set('n', 'h', '<cmd>echo "sorry"<CR>')
-vim.keymap.set('n', 'l', '<cmd>echo "sorry"<CR>')
+-- vim.keymap.set('n', 'j', '<cmd>echo "sorry"<CR>')
+-- vim.keymap.set('n', 'k', '<cmd>echo "sorry"<CR>')
+-- vim.keymap.set('n', 'h', '<cmd>echo "sorry"<CR>')
+-- vim.keymap.set('n', 'l', '<cmd>echo "sorry"<CR>')
 
 -- Keybinds to make split navigation easier.
 --  Use CTRL+<hjkl> to switch between windows
